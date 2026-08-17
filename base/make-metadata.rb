b4ac7e26594d62a3fa93d3ffaaba949a171d0e78
Charles Duan <cduan@sbf5.com>
2026-08-17 15:52:47 -0400

    Added module author metadata

---
+++ base/make-metadata.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+ARGF.each do |line|
+  dir, author, date = line.chomp.split(/\t/)
+  if File.directory?(dir)
+    open("#{dir}/metadata.tex", 'w') do |f|
+
+      f.puts("\\modauthor{#{author}}")
+      f.puts("\\moddate{#{date}}")
+    end
+  end
+end
